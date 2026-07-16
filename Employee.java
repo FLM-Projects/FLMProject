21ce57b4c8d2030fddc04f1d97af504aba2706cf
naseemashaik56 <sknaseema8122@gmail.com>
2026-07-16 15:37:34 +0530

    added new sub method

--- Employee.java
+++ Employee.java
@@ -4,4 +4,12 @@ public class Employee{
    System.out.println("Hii ...");
    System.out.println("Bye ...");
    }
+
+   public void sum(){
+     int a = 10;
+     int b = 20;
+     int c = a+b;
+     System.out.println(c);
+     System.out.println("Byr");
+   }
 }
